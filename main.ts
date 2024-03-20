fb5924b90a1f090e02e1a709603472096d918b60
zhaojun <88965869+luoqiu12138@users.noreply.github.com>
2024-03-20 17:20:19 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1636,7 +1636,7 @@ namespace lanqiaoqingshao{
         pins.i2cWriteNumber(0X57, 0X01, NumberFormat.UInt8BE)
         basic.pause(100)
         let readbuf = pins.i2cReadBuffer(0X57, pins.sizeOf(NumberFormat.UInt8LE) * 3)
-        cm = (readbuf[0] * 65535 + readbuf[1] * 256 + readbuf[2]) / 1000; 
+        cm = (readbuf[0] * 65535 + readbuf[1] * 256 + readbuf[2]) / 1000 /10; 
         Math.round(cm);
         return (cm)
     }
