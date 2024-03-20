93b78d0e836dd7c1ad0259ca241f6b22e68c65f1
zhaojun <88965869+luoqiu12138@users.noreply.github.com>
2024-03-20 11:18:43 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -33,6 +33,72 @@ enum NeoPixelMode {
     RGB_RGB = 3
 }
 
+const enum IrButton {
+    //% block="any"
+    Any = -1,
+    //% block=" "
+    Unused_2 = -2,
+    //% block=" "
+    Unused_3 = -2,
+    //% block="1"
+    Number_1 = 0xA2,
+    //% block="2"
+    Number_2 = 0x62,
+    //% block="3"
+    Number_3 = 0xE2,
+    //% block="4"
+    Number_4 = 0x22,
+    //% block="5"
+    Number_5 = 0x02,
+    //% block="6"
+    Number_6 = 0xC2,
+    //% block="7"
+    Number_7 = 0xE0,
+    //% block="8"
+    Number_8 = 0xA8,
+    //% block="9"
+    Number_9 = 0x90,
+    //% block="*"
+    Star = 0x68,
+    //% block="0"
+    Number_0 = 0x98,
+    //% block="#"
+    Hash = 0x80,
+    //% block=" "
+    Unused_4 = -2,
+    //% block="▲"
+    Up = 0x18,
+    //% block=" "
+    Unused_5 = -2,
+    //% block="◀"
+    Left = 0x10,
+    //% block="OK"
+    Ok = 0x38,
+    //% block="▶"
+    Right = 0x5A,
+    //% block=" "
+    Unused_6 = -3,
+    //% block="▼"
+    Down = 0x4A,
+    //% block=" "
+    Unused_7 = -4,
+
+}
+
+const enum IrButtonAction {
+    //% block="按下"
+    Pressed = 0,
+    //% block="松开"
+    Released = 1,
+}
+
+const enum IrProtocol {
+    //% block="Keyestudio"
+    Keyestudio = 0,
+    //% block="NEC"
+    NEC = 1,
+}
+
 //% weight=50 color=#0855AA icon="\uf26c" block="蓝桥青少"
 namespace lanqiaoqingshao{
 
@@ -935,5 +1001,633 @@ namespace lanqiaoqingshao{
         CounterClockwise,
         Shortest
     }
-  
+    
+    let COMMAND_I2C_ADDRESS = 0x24
+    let DISPLAY_I2C_ADDRESS = 0x34
+    let _SEG = [0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71];
+
+    let _intensity = 3
+    let dbuf = [0, 0, 0, 0]
+
+    /**
+     * send command to display
+     * @param is command, eg: 0
+     */
+    function cmd(c: number) {
+        pins.i2cWriteNumber(COMMAND_I2C_ADDRESS, c, NumberFormat.Int8BE)
+    }
+
+    /**
+     * send data to display
+     * @param is data, eg: 0
+     */
+    function dat(bit: number, d: number) {
+        pins.i2cWriteNumber(DISPLAY_I2C_ADDRESS + (bit % 4), d, NumberFormat.Int8BE)
+    }
+
+    /**
+     * 打开数码管显示
+     */
+    //% blockId="TM650_ON" block="开启显示"
+    //% weight=50 blockGap=8
+    //% subcategory=数码管
+    export function TM650_ON() {
+        cmd(_intensity * 16 + 1)
+    }
+
+    /**
+     * 关闭数码管显示
+     */
+    //% blockId="TM650_OFF" block="关闭显示"
+    //% weight=50 blockGap=8
+    //% subcategory=数码管
+    export function TM650_OFF() {
+        _intensity = 0
+        cmd(0)
+    }
+
+    /**
+     * 清空数码管显示
+     */
+    //% blockId="TM650_CLEAR" block="清空显示"
+    //% weight=40 blockGap=8
+    //% subcategory=数码管
+    export function TM650_CLEAR() {
+        dat(0, 0)
+        dat(1, 0)
+        dat(2, 0)
+        dat(3, 0)
+        dbuf = [0, 0, 0, 0]
+    }
+
+    /**
+     * 在指定位置显示
+     * @param digit is number (0-15) will be shown, eg: 1
+     * @param bit is position, eg: 0
+     */
+    //% blockId="TM650_DIGIT" block="显示数字%num|在第%bit位"
+    //% weight=80 blockGap=8
+    //% num.max=15 num.min=0
+    //% subcategory=数码管
+    export function digit(num: number, bit: number) {
+        dbuf[bit % 4] = _SEG[num % 16]
+        dat(bit, _SEG[num % 16])
+    }
+
+    /**
+     * 显示数字
+     * @param num is number will be shown, eg: 100
+     */
+    //% blockId="TM650_SHOW_NUMBER" block="显示数字%num"
+    //% weight=100 blockGap=8
+    //% subcategory=数码管
+    export function TM650_SHOW_NUMBER(num: number) {
+        if (num < 0) {
+            dat(0, 0x40) // '-'
+            num = -num
+        }
+        else
+            digit(Math.idiv(num, 1000) % 10, 0)
+        digit(num % 10, 3)
+        digit(Math.idiv(num, 10) % 10, 2)
+        digit(Math.idiv(num, 100) % 10, 1)
+    }
+
+    /**
+     * 显示16进制数字
+     * @param num is number will be shown, eg: 123
+     */
+    //% blockId="TM650_SHOW_HEX_NUMBER" block="显示16进制数字%num"
+    //% weight=90 blockGap=8
+    //% subcategory=数码管
+    export function TM650_SHOW_HEX_NUMBE(num: number) {
+        if (num < 0) {
+            dat(0, 0x40) // '-'
+            num = -num
+        }
+        else
+            digit((num >> 12) % 16, 0)
+        digit(num % 16, 3)
+        digit((num >> 4) % 16, 2)
+        digit((num >> 8) % 16, 1)
+    }
+
+    /**
+     * 显示点在指定位子
+     * @param bit is positiion, eg: 0
+     * @param show is true/false, eg: true
+     */
+    //% blockId="TM650_SHOW_DP" block="显示第%bit小数点|%num"
+    //% weight=80 blockGap=8
+    //% subcategory=数码管
+    export function showDpAt(bit: number, show: boolean) {
+        if (show) dat(bit, dbuf[bit % 4] | 0x80)
+        else dat(bit, dbuf[bit % 4] & 0x7F)
+    }
+
+    /**
+     * 显示亮度
+     * @param dat is intensity of the display, eg: 3
+     */
+    //% blockId="TM650_INTENSITY" block="设置显示亮度为dat"
+    //% weight=70 blockGap=8
+    //% subcategory=数码管
+    export function setIntensity(dat: number) {
+        if ((dat < 0) || (dat > 8))
+            return;
+        if (dat == 0)
+            TM650_OFF()
+        else {
+            _intensity = dat
+            cmd((dat << 4) | 0x01)
+        }
+    }
+
+    TM650_ON();
+
+    export enum Thread {
+        Priority = 0,
+        UserCallback = 1,
+    }
+
+    export enum Mode {
+        Repeat,
+        Once,
+    }
+
+    class Executor {
+        _newJobs: Job[] = undefined;
+        _jobsToRemove: number[] = undefined;
+        _pause: number = 100;
+        _type: Thread;
+
+        constructor(type: Thread) {
+            this._type = type;
+            this._newJobs = [];
+            this._jobsToRemove = [];
+            control.runInParallel(() => this.loop());
+        }
+
+        push(task: () => void, delay: number, mode: Mode): number {
+            if (delay > 0 && delay < this._pause && mode === Mode.Repeat) {
+                this._pause = Math.floor(delay);
+            }
+            const job = new Job(task, delay, mode);
+            this._newJobs.push(job);
+            return job.id;
+        }
+
+        cancel(jobId: number) {
+            this._jobsToRemove.push(jobId);
+        }
+
+        loop(): void {
+            const _jobs: Job[] = [];
+
+            let previous = control.millis();
+
+            while (true) {
+                const now = control.millis();
+                const delta = now - previous;
+                previous = now;
+
+                // Add new jobs
+                this._newJobs.forEach(function (job: Job, index: number) {
+                    _jobs.push(job);
+                });
+                this._newJobs = [];
+
+                // Cancel jobs
+                this._jobsToRemove.forEach(function (jobId: number, index: number) {
+                    for (let i = _jobs.length - 1; i >= 0; i--) {
+                        const job = _jobs[i];
+                        if (job.id == jobId) {
+                            _jobs.removeAt(i);
+                            break;
+                        }
+                    }
+                });
+                this._jobsToRemove = []
+
+
+                // Execute all jobs
+                if (this._type === Thread.Priority) {
+                    // newest first
+                    for (let i = _jobs.length - 1; i >= 0; i--) {
+                        if (_jobs[i].run(delta)) {
+                            this._jobsToRemove.push(_jobs[i].id)
+                        }
+                    }
+                } else {
+                    // Execute in order of schedule
+                    for (let i = 0; i < _jobs.length; i++) {
+                        if (_jobs[i].run(delta)) {
+                            this._jobsToRemove.push(_jobs[i].id)
+                        }
+                    }
+                }
+
+                basic.pause(this._pause);
+            }
+        }
+    }
+
+    class Job {
+        id: number;
+        func: () => void;
+        delay: number;
+        remaining: number;
+        mode: Mode;
+
+        constructor(func: () => void, delay: number, mode: Mode) {
+            this.id = randint(0, 2147483647)
+            this.func = func;
+            this.delay = delay;
+            this.remaining = delay;
+            this.mode = mode;
+        }
+
+        run(delta: number): boolean {
+            if (delta <= 0) {
+                return false;
+            }
+
+            this.remaining -= delta;
+            if (this.remaining > 0) {
+                return false;
+            }
+
+            switch (this.mode) {
+                case Mode.Once:
+                    this.func();
+                    basic.pause(0);
+                    return true;
+                case Mode.Repeat:
+                    this.func();
+                    this.remaining = this.delay;
+                    basic.pause(0);
+                    return false;
+            }
+        }
+    }
+
+    const queues: Executor[] = [];
+
+    export function schedule(
+        func: () => void,
+        type: Thread,
+        mode: Mode,
+        delay: number,
+    ): number {
+        if (!func || delay < 0) return 0;
+
+        if (!queues[type]) {
+            queues[type] = new Executor(type);
+        }
+
+        return queues[type].push(func, delay, mode);
+    }
+
+    export function remove(type: Thread, jobId: number): void {
+        if (queues[type]) {
+            queues[type].cancel(jobId);
+        }
+    }
+
+    let irState: IrState;
+
+    const IR_REPEAT = 256;
+    const IR_INCOMPLETE = 257;
+    const IR_DATAGRAM = 258;
+
+    const REPEAT_TIMEOUT_MS = 120;
+
+    interface IrState {
+        protocol: IrProtocol;
+        hasNewDatagram: boolean;
+        bitsReceived: uint8;
+        addressSectionBits: uint16;
+        commandSectionBits: uint16;
+        hiword: uint16;
+        loword: uint16;
+        activeCommand: number;
+        repeatTimeout: number;
+        onIrButtonPressed: IrButtonHandler[];
+        onIrButtonReleased: IrButtonHandler[];
+        onIrDatagram: () => void;
+    }
+    class IrButtonHandler {
+        irButton: IrButton;
+        onEvent: () => void;
+
+        constructor(
+            irButton: IrButton,
+            onEvent: () => void
+        ) {
+            this.irButton = irButton;
+            this.onEvent = onEvent;
+        }
+    }
+
+
+    function appendBitToDatagram(bit: number): number {
+        irState.bitsReceived += 1;
+
+        if (irState.bitsReceived <= 8) {
+            irState.hiword = (irState.hiword << 1) + bit;
+            if (irState.protocol === IrProtocol.Keyestudio && bit === 1) {
+                // recover from missing message bits at the beginning
+                // Keyestudio address is 0 and thus missing bits can be detected
+                // by checking for the first inverse address bit (which is a 1)
+                irState.bitsReceived = 9;
+                irState.hiword = 1;
+            }
+        } else if (irState.bitsReceived <= 16) {
+            irState.hiword = (irState.hiword << 1) + bit;
+        } else if (irState.bitsReceived <= 32) {
+            irState.loword = (irState.loword << 1) + bit;
+        }
+
+        if (irState.bitsReceived === 32) {
+            irState.addressSectionBits = irState.hiword & 0xffff;
+            irState.commandSectionBits = irState.loword & 0xffff;
+            return IR_DATAGRAM;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function decode(markAndSpace: number): number {
+        if (markAndSpace < 1600) {
+            // low bit
+            return appendBitToDatagram(0);
+        } else if (markAndSpace < 2700) {
+            // high bit
+            return appendBitToDatagram(1);
+        }
+
+        irState.bitsReceived = 0;
+
+        if (markAndSpace < 12500) {
+            // Repeat detected
+            return IR_REPEAT;
+        } else if (markAndSpace < 14500) {
+            // Start detected
+            return IR_INCOMPLETE;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function enableIrMarkSpaceDetection(pin: DigitalPin) {
+        pins.setPull(pin, PinPullMode.PullNone);
+
+        let mark = 0;
+        let space = 0;
+
+        pins.onPulsed(pin, PulseValue.Low, () => {
+            // HIGH, see https://github.com/microsoft/pxt-microbit/issues/1416
+            mark = pins.pulseDuration();
+        });
+
+        pins.onPulsed(pin, PulseValue.High, () => {
+            // LOW
+            space = pins.pulseDuration();
+            const status = decode(mark + space);
+
+            if (status !== IR_INCOMPLETE) {
+                handleIrEvent(status);
+            }
+        });
+    }
+
+    function handleIrEvent(irEvent: number) {
+
+        // Refresh repeat timer
+        if (irEvent === IR_DATAGRAM || irEvent === IR_REPEAT) {
+            irState.repeatTimeout = input.runningTime() + REPEAT_TIMEOUT_MS;
+        }
+
+        if (irEvent === IR_DATAGRAM) {
+            irState.hasNewDatagram = true;
+
+            if (irState.onIrDatagram) {
+                schedule(irState.onIrDatagram, Thread.UserCallback, Mode.Once, 0);
+            }
+
+            const newCommand = irState.commandSectionBits >> 8;
+
+            // Process a new command
+            if (newCommand !== irState.activeCommand) {
+
+                if (irState.activeCommand >= 0) {
+                    const releasedHandler = irState.onIrButtonReleased.find(h => h.irButton === irState.activeCommand || IrButton.Any === h.irButton);
+                    if (releasedHandler) {
+                        schedule(releasedHandler.onEvent, Thread.UserCallback, Mode.Once, 0);
+                    }
+                }
+
+                const pressedHandler = irState.onIrButtonPressed.find(h => h.irButton === newCommand || IrButton.Any === h.irButton);
+                if (pressedHandler) {
+                    schedule(pressedHandler.onEvent, Thread.UserCallback, Mode.Once, 0);
+                }
+
+                irState.activeCommand = newCommand;
+            }
+        }
+    }
+
+    function initIrState() {
+        if (irState) {
+            return;
+        }
+
+        irState = {
+            protocol: undefined,
+            bitsReceived: 0,
+            hasNewDatagram: false,
+            addressSectionBits: 0,
+            commandSectionBits: 0,
+            hiword: 0, // TODO replace with uint32
+            loword: 0,
+            activeCommand: -1,
+            repeatTimeout: 0,
+            onIrButtonPressed: [],
+            onIrButtonReleased: [],
+            onIrDatagram: undefined,
+        };
+    }
+
+    /**
+     * 在指定引脚连接红外接收模块，配置红外协议。
+     * @param pin IR receiver pin, eg: DigitalPin.P0
+     * @param protocol IR protocol, eg: IrProtocol.Keyestudio
+     */
+    //% blockId="makerbit_infrared_connect_receiver"
+    //% block="红外接收器引脚 %pin|设置解码方式为%protocol|"
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.columns=4
+    //% pin.fieldOptions.tooltips="false"
+    //% weight=90
+    //% subcategory=红外遥控
+    export function connectIrReceiver(
+        pin: DigitalPin,
+        protocol: IrProtocol
+    ): void {
+        initIrState();
+
+        if (irState.protocol) {
+            return;
+        }
+
+        irState.protocol = protocol;
+
+        enableIrMarkSpaceDetection(pin);
+
+        schedule(notifyIrEvents, Thread.Priority, Mode.Repeat, REPEAT_TIMEOUT_MS);
+    }
+
+    function notifyIrEvents() {
+        if (irState.activeCommand === -1) {
+            // skip to save CPU cylces
+        } else {
+            const now = input.runningTime();
+            if (now > irState.repeatTimeout) {
+                // repeat timed out
+
+                const handler = irState.onIrButtonReleased.find(h => h.irButton === irState.activeCommand || IrButton.Any === h.irButton);
+                if (handler) {
+                    schedule(handler.onEvent, Thread.UserCallback, Mode.Once, 0);
+                }
+
+                irState.bitsReceived = 0;
+                irState.activeCommand = -1;
+            }
+        }
+    }
+
+    /**
+     * 当遥控器特定按钮被按下或释放时 产生事件
+     * @param button the button to be checked
+     * @param action the trigger action
+     * @param handler body code to run when the event is raised
+     */
+    //% blockId=makerbit_infrared_on_ir_button
+    //% block="当按钮|%button|被%action时"
+    //% button.fieldEditor="gridpicker"
+    //% button.fieldOptions.columns=3
+    //% button.fieldOptions.tooltips="false"
+    //% weight=50
+    //% subcategory=红外遥控
+    export function onIrButton(
+        button: IrButton,
+        action: IrButtonAction,
+        handler: () => void
+    ) {
+        initIrState();
+        if (action === IrButtonAction.Pressed) {
+            irState.onIrButtonPressed.push(new IrButtonHandler(button, handler));
+        }
+        else {
+            irState.onIrButtonReleased.push(new IrButtonHandler(button, handler));
+        }
+    }
+
+    /**
+     * 返回最后按下的按钮的代码。如果还没有按钮被按下，返回-1。
+     */
+    //% blockId=makerbit_infrared_ir_button_pressed
+    //% block="红外接收值"
+    //% weight=70
+    //% subcategory=红外遥控
+    export function irButton(): number {
+        basic.pause(0); // Yield to support background processing when called in tight loops
+        if (!irState) {
+            return IrButton.Any;
+        }
+        return irState.commandSectionBits >> 8;
+    }
+
+    /**
+     * 接收到红外数据时发生事件
+     * @param handler body code to run when the event is raised
+     */
+    //% blockId=makerbit_infrared_on_ir_datagram
+    //% block="接收到红外数据时"
+    //% weight=40
+    //% subcategory=红外遥控
+    export function onIrDatagram(handler: () => void) {
+        initIrState();
+        irState.onIrDatagram = handler;
+    }
+
+    /**
+     * 返回32位十六进制字符串形式的IR数据报，当未接收到返回0x00000000
+     */
+
+    //% blockId=makerbit_infrared_ir_datagram
+    //% block="红外接收值(十六进制)"
+    //% weight=30
+    //% subcategory=红外遥控
+    export function irDatagram(): string {
+        basic.pause(0); // Yield to support background processing when called in tight loops
+        initIrState();
+        return (
+            "0x" +
+            ir_rec_to16BitHex(irState.addressSectionBits) +
+            ir_rec_to16BitHex(irState.commandSectionBits)
+        );
+    }
+
+    /**
+     * Returns true if any IR data was received since the last call of this function. False otherwise.
+     */
+
+    //% blockId=makerbit_infrared_was_any_ir_datagram_received
+    //% block="收到红外数据"
+    //% weight=80
+    //% subcategory=红外遥控
+    export function wasIrDataReceived(): boolean {
+        basic.pause(0); // Yield to support background processing when called in tight loops
+        initIrState();
+        if (irState.hasNewDatagram) {
+            irState.hasNewDatagram = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 返回特定按钮的值
+     * @param button the button
+     */
+    //% blockId=makerbit_infrared_button_code
+    //% button.fieldEditor="gridpicker"
+    //% button.fieldOptions.columns=3
+    //% button.fieldOptions.tooltips="false"
+    //% block="按钮%button的值"
+    //% weight=60
+    //% subcategory=红外遥控
+    export function irButtonCode(button: IrButton): number {
+        basic.pause(0); // Yield to support background processing when called in tight loops
+        return button as number;
+    }
+
+    function ir_rec_to16BitHex(value: number): string {
+        let hex = "";
+        for (let pos = 0; pos < 4; pos++) {
+            let remainder = value % 16;
+            if (remainder < 10) {
+                hex = remainder.toString() + hex;
+            } else {
+                hex = String.fromCharCode(55 + remainder) + hex;
+            }
+            value = Math.idiv(value, 16);
+        }
+        return hex;
+    }
+
+
+    
 }
